e43675893382df009dc702eec6b050bb781b8397
sv <sv@cloudgears.com>
2016-01-27 11:19:36 +0100

    1.0.7 (with initial meta-support for the calls)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject csi "0.1.6"
+(defproject csi "0.1.7"
  :description "ClojureScript interface to Erlang"
  :url "https://github.com/suprematic/csi.client"
  :license {:name "Eclipse Public License"
